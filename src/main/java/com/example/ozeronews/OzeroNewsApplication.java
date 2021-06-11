965126f927acd3ddfab213b93c8f5a35d1a15eb7
Denis Rossoshanskiy <Denis@MacBook-Air-Denis-2.local>
2021-06-11 13:09:07 +0300

    adding files

---
+++ src/main/java/com/example/ozeronews/OzeroNewsApplication.java
@@ -0,0 +1,13 @@
+package com.example.ozeronews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OzeroNewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OzeroNewsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ozeronews/OzeroNewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ozeronews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OzeroNewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
